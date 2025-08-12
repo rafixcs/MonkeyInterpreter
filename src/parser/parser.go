6ea9edf2a1f4b487755807f899371826afce6eb1
rafixcs <rafael.camargo.rs.@gmail.com>
2025-08-12 17:56:36 -0300

    lil fix

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -154,9 +154,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 
 	for p.curToken.Type != token.EOF {
 		stmt := p.parseStatement()
-		if stmt != nil {
-			program.Statements = append(program.Statements, stmt)
-		}
+		program.Statements = append(program.Statements, stmt)
 
 		p.nextToken()
 	}
